624e72bd9b149626f95fb981606f428e600fd804
Engineering TechSton <sufyanali69941@gmail.com>
2025-10-21 00:24:08 +0500

    Add files via upload

---
+++ Python calculator/Str_length checker.py
@@ -0,0 +1,11 @@
+
+
+def str_len(string):
+    count=0
+    for i in string:
+        count+=1
+    return count
+string=input ("Enter string to check length: ")
+print("The lenth of this string is ",str_len(string))
+    
+        
